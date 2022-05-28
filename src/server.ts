e17bac2b850b40fc8a43472c17405443d4c0b57e
prateek-moori <f20170668@pilani.bits-pilani.ac.in>
2022-05-28 17:59:56 +0530

    adding new pages

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import * as http from "http"
+import * as fs from "fs/promises"
 
-const server= http.createServer((req, res)=>{
-    console.log("request made");
+const server= http.createServer(async (req:any, res)=>{
+    console.log(req.url+" "+req.method);
+    res.setHeader("Content-type","text/html");
+    //send html file
+    try {
+        let fileread= await fs.readFile("./views/index.html");
+        res.write(fileread);
+    }
+    catch (error:any)
+    {
+        console.log(error);
+    }
+
+   // res.write("<p>Hi</p>");
+    res.end();
 })
 
 server.listen(3000,'localhost',()=>{
